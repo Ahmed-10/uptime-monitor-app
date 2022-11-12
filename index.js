e127657328e2f4006a4297737187cf4a002a50eb
ahmed-10 <ahmedalaa.10@hotmail.com>
2022-11-12 04:10:38 +0200

    parsing request url

--- index.js
+++ index.js
@@ -5,10 +5,18 @@
 
 // Dependencies
 const http = require('http')
+const url = require('url')
 
 // the server response to API requests
 const server = http.createServer(function(req, res){
+	const parsedUrl = url.parse(req.url, true)
+	// cleaning pathname from prefixed and postfixed slashes
+	const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '')
+	
 	res.end('hello world!\n')
+
+	console.log(`Request received on path: ${parsedUrl.pathname}`)
+	console.log(`path after trimming: ${trimmedPath}`)
 })
 
 // start the server, listening to port 3000
